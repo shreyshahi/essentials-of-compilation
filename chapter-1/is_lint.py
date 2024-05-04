ff96ede5de6348f34ac80cae60d86b9a97d9fe08
Shrey Shahi <shrey.shahi@gmail.com>
2024-05-03 17:03:55 -0700

    wrote is_lint program

--- chapter-1/is_lint.py
+++ chapter-1/is_lint.py
@@ -1,6 +1,37 @@
 import ast
 
 # This helps check if a program is a valid L_int program 
-#
+# Notes for Chapter 1 in README has the context free grammer for L_int
 
-def is_lint()
+def is_exp(exp):
+    match exp:
+        case ast.Constant(x):
+            return True
+        case ast.Call(ast.Name("input_int", [])):
+            return True
+        case ast.UnaryOp(ast.USub(e)):
+            return is_exp(e)
+        case ast.BinOp(e1, ast.Add(), e2):
+            return is_exp(e1) and is_exp(e2)
+        case ast.BinOp(e1, ast.Sub(), e2):
+            return is_exp(e1) and is_exp(e2)
+        case _:
+            return False
+
+
+def is_stmt(s):
+    match s:
+        case ast.Expr(ast.print(e)):
+            return is_exp(e)
+        case ast.Expr(e):
+            return is_exp(e)
+        case _:
+            return False
+
+
+def is_lint(program):
+    match program:
+        case ast.Module(list_of_stmt):
+            return all([is_stmt(s) for s in list_of_stmt])
+        case _:
+            return False
